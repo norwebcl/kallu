9842fc3d3da701fb3252ad9d8d43c66312dd1ba1
NORWEB <contacto@norweb.cl>
2021-04-15 10:59:19 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,5 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "xxx-xxx-xxx",
-    authDomain: "xxx",
-    databaseURL: "xxx",
-    projectId: "xxx",
-    storageBucket: "xxx",
-    messagingSenderId: "xxx",
-    appId: "xxx",
-    measurementId: "xxx"
   },
 };
